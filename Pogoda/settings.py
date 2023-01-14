021d6abc82b9a60cb95ebf1190107b6241e191b9
Ruzakiiii <alex.yarosh.2005@gmail.com>
2023-01-14 11:48:40 +0300

    first commit

--- Pogoda/settings.py
+++ Pogoda/settings.py
@@ -132,3 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Для отоброжения дизайна
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
